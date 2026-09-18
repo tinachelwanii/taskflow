a70104c0a0ff67eec367f63e9834a0e4848ebaea
Tina <chelwanit13@gmail.com>
2026-09-18 14:27:17 +0530

    feat: add user authentication with JWT

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -10,14 +11,14 @@ connectDB();
 
 const app = express();
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Test Route
 app.get("/", (req, res) => {
   res.send("TaskFlow API is running");
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
